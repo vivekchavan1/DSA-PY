c7067c8b7152f927489a252266f16aa5c2bf89f2
vivekchavan1 <chavanvivek7722@gmail.com>
2026-03-04 12:31:59 +0000

    04/03/26

---
+++ linked_list.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+node1 = Node(3)
+node2 = Node(15)
+node3 = Node(5)
+node4 = Node(2)  
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+currentNode = node1
+while currentNode:
+    print(currentNode.data, end=" -> ")
+    currentNode = currentNode.next
+print("null")
+
+
+
+
