29e08c50f381f48cab99a5c2086da4aff5453bb0
vivekchavan1 <chavanvivek7722@gmail.com>
2026-04-01 12:05:56 +0000

    1/04/26

--- linked_list.py
+++ linked_list.py
@@ -1,23 +1,88 @@
+# #Singly Linked List
+
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+
+# node1 = Node(3)
+# node2 = Node(15)
+# node3 = Node(5)
+# node4 = Node(2)  
+
+# node1.next = node2
+# node2.next = node3
+# node3.next = node4
+
+# currentNode = node1
+# while currentNode:
+#     print(currentNode.data, end=" -> ")
+#     currentNode = currentNode.next
+# print("null")
+
+
+# Doubly Linked List
+
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+#         self.prev = None
+    
+# node1 = Node(3)
+# node2 = Node(5)
+# node3 = Node(13)
+# node4 = Node(2)
+
+# node1.next = node2
+
+# node2.prev = node1
+# node2.next = node3
+
+# node3.prev = node2
+# node3.next = node4
+
+# node4.prev = node3
+
+# print("\nTraversing forward:")
+# currentNode = node1
+# while currentNode:
+#     print(currentNode.data, end=" -> ")
+#     currentNode = currentNode.next
+# print("null")
+
+# print("\nTraversing backward:")
+# currentNode = node4
+# while currentNode:
+#     print(currentNode.data, end=" -> ")
+#     currentNode = currentNode.prev 
+# print("null") 
+
+
+#Circular Linked List
+
 class Node:
-    def __init__(self, data):
+    def __init__(self,data):
         self.data = data
         self.next = None
 
 node1 = Node(3)
-node2 = Node(15)
-node3 = Node(5)
-node4 = Node(2)  
+node2 = Node(5)
+node3 = Node(13)
+node4 = Node(2)
 
 node1.next = node2
 node2.next = node3
 node3.next = node4
+node4.next = node1
 
 currentNode = node1
-while currentNode:
+startNode = node1
+print(currentNode.data, end=" -> ") 
+currentNode = currentNode.next 
+
+while currentNode != startNode:
     print(currentNode.data, end=" -> ")
     currentNode = currentNode.next
-print("null")
 
-
-
-
+print("...")
